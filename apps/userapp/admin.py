428ac852754e47c4123b993734159be04df7164e
disen <610039018@qq.com>
2019-01-22 17:48:21 +0800

    修改userprofile的后台form布局

--- apps/userapp/admin.py
+++ apps/userapp/admin.py
@@ -1,7 +1,12 @@
+from crispy_forms.bootstrap import AppendedText
+from crispy_forms.layout import Row
+
 import xadmin as admin
 from xadmin import views
 
 from userapp.models import UserProfile
+from xadmin.layout import Main, TabHolder, Tab, Fieldset, Side, HTML, Col, Column
+from xadmin.plugins.inline import Inline
 
 
 @admin.sites.register(views.BaseAdminView)
@@ -24,8 +29,25 @@ class GlobalSetting(object):
 
 @admin.sites.register(UserProfile)
 class UserProfileAdmin(object):
+    def img_path(self, obj):
+        return "/static/"+obj.photo
+
     list_display = ('username', 'email', 'phone', 'last_time')
     list_display_links = ('username', )
     list_filter = ['username']
 
     search_fields = ['username', 'phone']
+
+    form_layout = (
+        Main(
+            Fieldset("用户与口令", "username", 'password', description="required"),
+            Fieldset('显示图片', 'photo', HTML('<img width=100 height=120 style="margin:10px;" src="/static/{{ form.photo.value }}">')),
+            Fieldset(
+                "邮箱与电话",
+                'phone',
+                'email'
+            )
+        ),
+    )
+
+    reversion_enable = True
